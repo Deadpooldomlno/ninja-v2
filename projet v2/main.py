e9afbb35c3bda52e395e84270e2f00b11de6036e
Deadpooldomlno <apopathodia@gmail.com>
2025-03-12 09:37:19 +0100

    Update main.py

--- projet v2/main.py
+++ projet v2/main.py
@@ -1,86 +1,47 @@
+from game import Game
 import pygame
 pygame.init()
 
-class Player():
+plateformes = [
+    pygame.Rect(200, 500, 400, 20),
+    pygame.Rect(100, 400, 200, 20),
+    pygame.Rect(500, 300, 200, 20),
+    pygame.Rect(50, 100, 100, 20)
+]
+
+game = Game()
+
+screen = pygame.display.set_mode((game.w, game.h))
+pygame.display.set_caption('ninja v2')
+
+fps= pygame.time.Clock()
+
+running = True
+while running :
+    fps.tick(60)
     
-    def __init__(self,game):
-        self.game = game
-        
-        self.surf = pygame.Surface((40,40))
-        self.surf.fill((0,0,255))
-        self.rect = self.surf.get_rect(center = (self.game.w/2,self.game.h/2))
-        
-        self.vx = 0 
-        self.vy = 0
-        
-        self.peut_sauter = False
-        
-        self.vitesse_deplacement  = 5
-        self.force_saut = -10
-        self.gravite = 0.5
-        self.vitesse_max_chute = 12
-        
-    def deplacer_gauche(self):
-        self.vx = -self.vitesse_deplacement 
-        
-    def deplacer_droite(self):
-        self.vx = self.vitesse_deplacement 
-        
-    def arreter(self):
-        self.vx = 0
-        
-    def sauter(self):
-        if self.peut_sauter:
-            self.vy = self.force_saut
-        
-    def update(self, plateformes):
-        self.vy += self.gravite
-        self.vy = min(self.vy, self.vitesse_max_chute)
-
-        self.rect.x += self.vx
-        
-        for plateforme in plateformes:
-            if self.rect.colliderect(plateforme):
-                if self.vx > 0:
-                    self.rect.right = plateforme.left
-                elif self.vx < 0:  
-                    self.rect.left = plateforme.right
-        
-        if self.rect.left < 0:
-            self.rect.left = 0
-            self.arreter()
-        elif self.rect.right > self.game.w:
-            self.rect.right = self.game.w
-            self.arreter()
-        
-        self.rect.y += self.vy
-        
-        #self.peut_sauter = False
-        
-        for plateforme in plateformes:
-            if self.rect.colliderect(plateforme):
-                if self.vy > 0: 
-                    self.rect.bottom = plateforme.top
-                    self.vy = 0 
-                    self.peut_sauter = True
-                elif self.vy < 0: 
-                    self.rect.top = plateforme.bottom
-                    self.vy = self.gravite
-                    
-        if self.rect.top < 0:
-            self.rect.top = 0
-            self.vy = self.gravite
-        elif self.rect.bottom > self.game.h:
-            self.rect.bottom = self.game.h
-            self.vy = 0
-            self.peut_sauter = True
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                game.player.sauter()
             
-            
-                    
-        
+    k = pygame.key.get_pressed()
 
-        
+    if k[pygame.K_LEFT]:
+        game.player.deplacer_gauche()
+    elif k[pygame.K_RIGHT]:
+        game.player.deplacer_droite()
+    else:
+        game.player.arreter()
     
-
-        
-        
+    screen.fill((255,255,255))
+    screen.blit(game.player.surf, game.player.rect)
+    game.player.update(plateformes)
+    
+    for plateforme in plateformes:
+        pygame.draw.rect(screen, (0,0,0), plateforme)
+    
+    pygame.display.update()
+pygame.quit()
