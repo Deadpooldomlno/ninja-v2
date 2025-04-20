fe000a69710b8dba07fad5b94fe1c7e1f7397344
Deadpooldomlno <apopathodia@gmail.com>
2025-04-20 13:13:52 +0200

    Update game.py

--- ninja-v2-main/game.py
+++ ninja-v2-main/game.py
@@ -1,3 +1,4 @@
+from button import *
 from player import Player
 from ennemy import Ennemy
 import pygame
@@ -22,7 +23,27 @@ class Game:
         self.ennemies.append(self.ennemy.rect)
         self.players=[]
         self.players.append(self.player.rect)
+        
+        #boutons
+        self.button_setting = Button(screen,('assets/buttons/setting1.png','assets/buttons/setting2.png'),(self.w-30,20))
+        self.button_restart = Button(screen,('assets/buttons/restart1.png','assets/buttons/restart2.png'),(self.w-66,20))
+        self.button_play = Button(screen,('assets/buttons/play1.png','assets/buttons/play2.png'),(self.w/2,self.h/2),True)
+        self.button_menu = Button(screen,('assets/buttons/menu1.png','assets/buttons/menu2.png'),(self.w/2,self.h/2-90),True)
+        self.button_quit = Button(screen,('assets/buttons/exit1.png','assets/buttons/exit2.png'),(self.w/2,self.h/2+90),True)
+        
+        #textes
+        self.death = Texte(screen,'assets/death.png',(57,20),True)
+        self.timer = Texte(screen,'assets/timer.png',(355,20),True)
+        self.bestTime = Texte(screen,'assets/bestTime.png',(85,20),True)
 
+    #desine une bande grise
+    def bande_grise(self):
+        surf = pygame.Surface((self.w,40))
+        surf.fill((127,127,127))
+        surf_rect=surf.get_rect()
+        self.screen.blit(surf,surf_rect)
+    
+    #reinitialise certins atributs
     def reset(self):
         self.player = Player(self)
         self.ennemy = Ennemy(self,50,1)
@@ -33,6 +54,3 @@ class Game:
         self.players=[]
         self.players.append(self.player.rect)
         
-        
-        
-    
