32761794ca42bf289994161feca132d7e452c62b
Deadpooldomlno <apopathodia@gmail.com>
2025-04-20 02:19:13 +0200

    Update game.py

--- ninja-v2-main/game.py
+++ ninja-v2-main/game.py
@@ -1,6 +1,5 @@
 from player import Player
 from ennemy import Ennemy
-#from background import Background
 import pygame
 pygame.init()
 
@@ -11,10 +10,29 @@ class Game:
         self.w = screen.get_width()
         self.h = screen.get_height()
         self.gravite = 0.5
-        self.vitesse_max_chute = 12
-
-        #self.bg = Background(self)       
+        self.vitesse_max_chute = 12      
+        
+        self.nb_mort =0
         
         self.player = Player(self)
         self.ennemy = Ennemy(self,50,1)
+        self.plateformes = [pygame.Rect(200, 500, 400, 20),pygame.Rect(100, 400, 200, 20),pygame.Rect(520, 325, 200, 20)]
+   
+        self.ennemies = []
+        self.ennemies.append(self.ennemy.rect)
+        self.players=[]
+        self.players.append(self.player.rect)
+
+    def reset(self):
+        self.player = Player(self)
+        self.ennemy = Ennemy(self,50,1)
+        self.plateformes = [pygame.Rect(200, 510, 400, 20),pygame.Rect(100, 410, 200, 20),pygame.Rect(520, 335, 200, 20)]
+   
+        self.ennemies = []
+        self.ennemies.append(self.ennemy.rect)
+        self.players=[]
+        self.players.append(self.player.rect)
+        
+        
+        
     
